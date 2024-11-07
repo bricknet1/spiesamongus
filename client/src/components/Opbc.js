2574960aa4170bf0023f7aa3a1f312c75675b03c
Nick Johnson <nlj222@gmail.com>
2024-11-06 19:32:39 -0800

    added specific password to opbc page, started adding content to correct password entry

--- client/src/components/Opbc.js
+++ client/src/components/Opbc.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import papyrusheadshake3 from '../assets/pictures/Papyrusheadshake3.gif';
+import opbcimage from '../assets/pictures/Operation Bubonic Curtsy 3.20.24.jpg'
 
 function Opbc() {
 
@@ -22,29 +24,50 @@ function Opbc() {
 
       <div className='orangeBar'>OPERATION BUBONIC CURTSY</div>
 
-      <div className='opbc-subheader'>WARNING! Classified files. Enter password to continue.</div>
+      {(submittedPassword !== "egregious" || submittedPassword === "") && <div>
 
+        <div className='opbc-subheader'>WARNING! Classified files. Enter password to continue.</div>
 
+        <form onSubmit={handleSubmit} className="opbcPageForm">
 
-      <form onSubmit={handleSubmit} className="opbcPageForm">
+          <input
+            type="text" 
+            name="password"
+            className="passwordFormField"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <input
-          type="text" 
-          name="password"
-          className="passwordFormField"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <button type="submit" className="passwordButton">Enter</button>
 
-        <button type="submit" className="passwordButton">Enter</button>
+        </form>
 
-      </form>
+      </div>}
 
+      {submittedPassword==="" && <div></div>}
 
+      {(submittedPassword !== "egregious" && submittedPassword !== "") && 
+        <div className='wrongPassword'>Wrong password.<br/>Entry denied!<br/>
+          <img src={papyrusheadshake3} alt="Papyrus shaking his head" className="papyrusheadshake3"/>
+        </div>
+      }
 
-      {submittedPassword==="test" && <div className='orangeBar'>test successful</div>}
-      {submittedPassword==="test2" && <div className='orangeBar'>test 2 successful</div>}
-      {submittedPassword==="egregious" && <div className='orangeBar'>egregious</div>}
+      {submittedPassword==="egregious" && 
+        <div className='rightPassword'>
+          <br/>
+          <img src={opbcimage} alt="Secret Document" className="opbcimage"/>
+          <br/><br/>
+          <div className='orangeBar'>FALLOUT FROM OP BC</div>
+          <br/>
+          <div className='killOrder'>KILL ORDER PHONE CALL<br/>(five years ago)</div>
+          <br/>
+          <div className='describeCall'>Recording of official call between Special Agent PAPYRUS and LESLIE, Head of Human Resources</div>
+          <br/>
+          <div className="videoContainer">
+            <iframe className="video" src="https://video.wixstatic.com/video/aa1885_21dd3df0aed746ce9cc32b5971c58e86/720p/mp4/file.mp4" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+          </div>
+        </div>
+      }
 
 
     </div>
