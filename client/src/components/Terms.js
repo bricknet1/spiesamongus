f198da1dc6aac2d876df558ca9c245592378d7c6
Nick Johnson <nlj222@gmail.com>
2024-11-11 19:01:24 -0800

    started building terms page

--- client/src/components/Terms.js
+++ client/src/components/Terms.js
@@ -1,8 +1,71 @@
+import instagram from '../assets/pictures/instagram.webp';
+
 function Terms() {
 
   return (
     <div>
-      Future home of the terms and conditions
+      <title>Terms & Conditions</title>
+
+      <div className='orangeBar'>TERMS & CONDITIONS</div>
+
+      <div>
+
+        Participant Agreement and Release of Liability Waiver
+
+        <ol>
+
+        Assumption of Risk:
+        You understand that participating in the experience involves certain risks, including but not limited to physical exertion, exposure to weather conditions, and potential hazards associated with traffic and public spaces.
+
+        You voluntarily assume all risks associated with your participation in this event.
+
+        Traffic Laws and Safety:
+        You agree to follow all local, state, and federal traffic laws and regulations during the experience.
+
+        You will exercise caution when crossing streets, using crosswalks, and interacting with vehicular traffic.
+
+        Responsible Behavior
+        You will treat all actors, event staff, and members of the general public with respect and politeness.
+
+        You understand that you may mistakenly interact with a member of the general public while searching for the actor. You will politely leave them alone if so.
+
+        Release of Liability:
+        In consideration of being allowed to participate in the experience, you hereby release and discharge Interactive Escapes LLC, its officers, volunteers, sponsors, and affiliates from any and all claims, liabilities, demands, actions, or causes of action arising out of your participation in the event.
+
+        You waive any right to seek compensation for injuries, damages, or losses incurred during the experience.
+
+        Use of Image in Marketing:
+        You grant permission to Interactive Escapes LLC to use your likeness, including photographs and videos taken during the event, for promotional purposes.
+
+        You understand that your image may appear in posts, advertisements, and other promotional materials and waive any right to compensation for such use.
+
+        Sharing the Waiver with Other Participants:
+        You agree to share this liability waiver with all other members of your party who are participating in the experience.
+
+        It is your responsibility to ensure that everyone in your group is aware of and agrees to the terms outlined in this waiver.
+
+        Expectations of the Experience
+        You will receive texts, receive physical objects, and be directed where to walk from texts and websites on your phone.
+
+        No one will ever approach you and tell you to:
+
+        Go somewhere.
+
+        Get into a vehicle.
+
+        Give them money.
+
+        SMS/MMS Mobile Messages​
+
+        By signing up, you agree to receive recurring automated text messages from Interactive Escapes LLC. Reply STOP to cancel. Msg frequency varies. Msg & data rates may apply.
+
+        </ol>
+
+      </div>
+
+      <a href='https://www.instagram.com/spiesamong' target='_blank'><img src={instagram} style={{ width: '10vw', marginBottom: '0', paddingBottom: '0'}}></img></a>
+      <div style={{ fontSize: '5vw'}}>2024 Interactive Escapes</div>
+
     </div>
   )
 }
