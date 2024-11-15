a54a98b3af58656c3a2427a8b23c62a5c39abd92
Nick Johnson <nlj222@gmail.com>
2024-11-14 18:56:36 -0800

    built terms page

--- client/src/components/Terms.js
+++ client/src/components/Terms.js
@@ -10,61 +10,72 @@ function Terms() {
 
       <div>
 
-        Participant Agreement and Release of Liability Waiver
+        <div className='termsHeading'>Participant Agreement and Release of Liability Waiver</div>
 
         <ol>
 
-        Assumption of Risk:
-        You understand that participating in the experience involves certain risks, including but not limited to physical exertion, exposure to weather conditions, and potential hazards associated with traffic and public spaces.
+          <li className='termsOL'>Assumption of Risk:</li>
+          <ul>
+            <li className='termsUL'>You understand that participating in the experience involves certain risks, including but not limited to physical exertion, exposure to weather conditions, and potential hazards associated with traffic and public spaces.</li>
+            <li className='termsUL'>You voluntarily assume all risks associated with your participation in this event.</li>
+          </ul>
 
-        You voluntarily assume all risks associated with your participation in this event.
+          <li className='termsOL'>Traffic Laws and Safety:</li>
+          <ul>
+            <li className='termsUL'>You agree to follow all local, state, and federal traffic laws and regulations during the experience.</li>
+            <li className='termsUL'>You will exercise caution when crossing streets, using crosswalks, and interacting with vehicular traffic.</li>
+          </ul>
 
-        Traffic Laws and Safety:
-        You agree to follow all local, state, and federal traffic laws and regulations during the experience.
 
-        You will exercise caution when crossing streets, using crosswalks, and interacting with vehicular traffic.
+          <li className='termsOL'>Responsible Behavior</li>
+          <ul>
+            <li className='termsUL'>You will treat all actors, event staff, and members of the general public with respect and politeness.</li>
+            <li className='termsUL'>You understand that you may mistakenly interact with a member of the general public while searching for the actor. You will politely leave them alone if so.</li>
+          </ul>
 
-        Responsible Behavior
-        You will treat all actors, event staff, and members of the general public with respect and politeness.
+          <li className='termsOL'>Release of Liability:</li>
+          <ul>
+            <li className='termsUL'>In consideration of being allowed to participate in the experience, you hereby release and discharge Interactive Escapes LLC, its officers, volunteers, sponsors, and affiliates from any and all claims, liabilities, demands, actions, or causes of action arising out of your participation in the event.</li>
+            <li className='termsUL'>You waive any right to seek compensation for injuries, damages, or losses incurred during the experience.</li>
+          </ul>
 
-        You understand that you may mistakenly interact with a member of the general public while searching for the actor. You will politely leave them alone if so.
+          <li className='termsOL'>Use of Image in Marketing:</li>
+          <ul>
+            <li className='termsUL'>You grant permission to Interactive Escapes LLC to use your likeness, including photographs and videos taken during the event, for promotional purposes.</li>
+            <li className='termsUL'>You understand that your image may appear in posts, advertisements, and other promotional materials and waive any right to compensation for such use.</li>
+          </ul>
 
-        Release of Liability:
-        In consideration of being allowed to participate in the experience, you hereby release and discharge Interactive Escapes LLC, its officers, volunteers, sponsors, and affiliates from any and all claims, liabilities, demands, actions, or causes of action arising out of your participation in the event.
+          <li className='termsOL'>Sharing the Waiver with Other Participants:</li>
+          <ul>
+            <li className='termsUL'>You agree to share this liability waiver with all other members of your party who are participating in the experience.</li>
+            <li className='termsUL'>It is your responsibility to ensure that everyone in your group is aware of and agrees to the terms outlined in this waiver.</li>
+          </ul>
 
-        You waive any right to seek compensation for injuries, damages, or losses incurred during the experience.
+          <li className='termsOL'>Expectations of the Experience</li>
+          <ul>
+            <li className='termsUL'>You will receive texts, receive physical objects, and be directed where to walk from texts and websites on your phone.</li>
 
-        Use of Image in Marketing:
-        You grant permission to Interactive Escapes LLC to use your likeness, including photographs and videos taken during the event, for promotional purposes.
+            <li className='termsUL'>No one will ever approach you and tell you to:
+              <ol>
+                <li className='termsUL'>Go somewhere.</li>
+                <li className='termsUL'>Get into a vehicle.</li>
+                <li className='termsUL'>Give them money.</li>
+              </ol>
+            </li>
+          </ul>
 
-        You understand that your image may appear in posts, advertisements, and other promotional materials and waive any right to compensation for such use.
-
-        Sharing the Waiver with Other Participants:
-        You agree to share this liability waiver with all other members of your party who are participating in the experience.
-
-        It is your responsibility to ensure that everyone in your group is aware of and agrees to the terms outlined in this waiver.
-
-        Expectations of the Experience
-        You will receive texts, receive physical objects, and be directed where to walk from texts and websites on your phone.
-
-        No one will ever approach you and tell you to:
-
-        Go somewhere.
-
-        Get into a vehicle.
-
-        Give them money.
-
-        SMS/MMS Mobile Messages​
-
-        By signing up, you agree to receive recurring automated text messages from Interactive Escapes LLC. Reply STOP to cancel. Msg frequency varies. Msg & data rates may apply.
+          <li className='termsOL'>SMS/MMS Mobile Messages​</li>
+          <ul>
+          <li className='termsUL'>By signing up, you agree to receive recurring automated text messages from Interactive Escapes LLC. Reply STOP to cancel. Msg frequency varies. Msg & data rates may apply.</li>
+          </ul>
 
         </ol>
 
       </div>
-
-      <a href='https://www.instagram.com/spiesamong' target='_blank'><img src={instagram} style={{ width: '10vw', marginBottom: '0', paddingBottom: '0'}}></img></a>
-      <div style={{ fontSize: '5vw'}}>2024 Interactive Escapes</div>
+      <div style={{ fontSize: '5vw', textAlign: 'center', marginTop: '20vw', marginBottom: '10vw'}}>
+        <a href='https://www.instagram.com/spiesamong' target='_blank'><img src={instagram} style={{ width: '10vw', marginBottom: '0', paddingBottom: '0'}}></img></a>
+        <div>2024 Interactive Escapes</div>
+      </div>
 
     </div>
   )
