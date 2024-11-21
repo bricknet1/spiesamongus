de8856c0416c1f783cb84e51fcc1bdc973d30eaa
Nick Johnson <nlj222@gmail.com>
2024-11-20 20:52:50 -0800

    built obeliskterms page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -6,8 +6,8 @@ function NotFound() {
     <div className='pageContent' style={{ fontSize: '10vw', margin: '0 auto', width: '90vw', textAlign: 'center' }}>
       <title>Spies Among Us</title>
       <br/>THERE'S NOTHING HERE AGENT...<br/><br/><br/><br/><br/><br/><br/><br/>
-      <a href='https://www.instagram.com/spiesamong' target='_blank'><img src={instagram} style={{ width: '10vw', marginBottom: '0', paddingBottom: '0'}}></img></a>
-      <div style={{ fontSize: '5vw'}}>2024 Interactive Escapes</div>
+      <a href='https://www.instagram.com/spiesamong' target='_blank' rel="noreferrer"><img src={instagram} alt="Instagram" style={{ width: '10vw', marginBottom: '0', paddingBottom: '0'}}></img></a>
+      <div style={{ fontSize: '5vw', fontWeight: "normal"}} >2024 Interactive Escapes</div>
     </div>
   )
 }
--- client/src/components/ObeliskTerms.js
+++ client/src/components/ObeliskTerms.js
@@ -1,8 +1,61 @@
 function ObeliskTerms() {
 
   return (
-    <div>
-      Future home of the Obelisk terms and conditions
+    <div style={{backgroundColor: 'black', paddingBottom: '20vw'}}>
+      <title>Obelisk Terms</title>
+
+      <div className='yellowBar'>OBELISK TERMS & CONDITIONS</div>
+
+      <div className='termsContainer'>
+
+        <div className='termsHeading'>Participant Agreement and Release of Liability Waiver</div>
+
+        <ol>
+
+          <li className='termsOL'>Data Sharing</li>
+          <ul>
+            <li className='termsUL'>By placing your hands on the Obelisk, you agree to share all your data with the Obelisk.</li>
+          </ul>
+
+          <li className='termsOL'>Power of Attorney</li>
+          <ul>
+            <li className='termsUL'>You agree the power of a measly attorney is nothing compared to the power of an all-knowing obelisk (with a law degree).</li>
+          </ul>
+
+
+          <li className='termsOL'>Responsible Behavior</li>
+          <ul>
+            <li className='termsUL'>You acknowledge that the Obelisk appreciates:
+              <ul>
+                <li>Being bowed to</li>
+                <li>Compliments</li>
+                <li>Animal sacrifices (no cloven hooves!)</li>
+                <li>Being embraced</li>
+              </ul>
+            </li>
+          </ul>
+
+          <li className='termsOL'>Release of Liability</li>
+          <ul>
+            <li className='termsUL'>You agree to help Liability, the Obelisk's niece, escape from silver statue prison</li>
+          </ul>
+
+          <li className='termsOL'>Washington Monument</li>
+          <ul>
+            <li className='termsUL'>You condemn that the Washington Monument was picked to star in Spiderman: Homecoming when the Obelisk would have done a superior job.</li>
+          </ul>
+
+          <li className='termsOL'>Opt Out</li>
+          <ul>
+            <li className='termsUL'>You may opt out of this waiver by texting Papyrus's phone number with the opt out phrase "No way, Jose."</li>
+            <li className='termsUL'>You must do this before placing your hands on the Obelisk.</li>
+          </ul>
+          
+        </ol>
+
+      </div>
+      
+
     </div>
   )
 }
