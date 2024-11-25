423f91f9d5834e2213f98417b91320c1cee579e5
Nick Johnson <nlj222@gmail.com>
2024-11-24 16:24:55 -0800

    finished yourupdatedmission with the exception of some pissing picture assets

--- client/src/components/YourUpdatedMission.js
+++ client/src/components/YourUpdatedMission.js
@@ -1,4 +1,4 @@
-import pathPic from '../assets/pictures/Marble Polaroid.png';
+import mapPic from '../assets/pictures/Marble Polaroid.png';
 
 import LeslieFooter from "./LeslieFooter.js";
 
@@ -18,40 +18,51 @@ function YourUpdatedMission() {
         <br/>
         We recommend walking in the <span className='mission-emphasis'>opposite direction</span> of Marble, to increase your chances of finding them <span className='mission-emphasis'>OR</span> pick a <span className='mission-emphasis'>secluded spot</span> to stake out and wait for Marble to approach.</div><br/>
 
+        <div className="updatedMission-map-wrapper">
+          <img src={mapPic} className="updatedMission-map" alt="Agent Marble" />
+        </div>
+
         <div className='orangeBar'>YOUR UPDATED MISSION</div>
         <br/>
         <br/>
 
         <div className='mission-subsubheader'>
 
-          <span className='mission-emphasis'>1. TRACK DOWN AGENT MARBLE.</span> If found, do not engage.
-
-          <img src={pathPic} className="mission-polaroid" alt="Agent Marble" />
-
-          <span className='mission-emphasis'>2. STAY UNDERCOVER and STICK TOGETHER.</span> This area is a hive of spies and you do not want to draw attention to yourself or your targets. Act like you are normal shoppers at all times.<br/>
-
-          
-
-          <span className='mission-emphasis'>3. AVOID THESE WORDS:</span> Texting the words STOP or UNSUBSCRIBE will abruptly end your mission.<br/>
+          <span className='mission-emphasis'>1. STAY UNDERCOVER.</span> Do not draw attention to yourself or your target. Act like you are a normal shopper at all times.<br/>
           <br/>
 
           <div className="orangeLine"/><br/>
+
+          <span className='mission-emphasis'>2. TRACK DOWN AGENT MARBLE.</span> If found:<br/>
+
+          <div style={{ textAlign: 'center'}} >Say:</div>
+
+          <span className='mission-emphasis' style={{ color: '#F9DF39'}}><i>"Sorry, do you know how to get to Pershing Square?"</i></span><br/>
           <br/>
 
-          <span className='mission-emphasis'><u>4. DOWNLOAD THIS CONTACT FILE TO YOUR PHONE</u> and follow your phone's instructions to import them into your contacts. Failure to do this will make your mission VERY confusing.</span>
+          <div style={{ textAlign: 'center'}} >Marble will respond:</div>
 
+          <span className='mission-emphasis' style={{ color: '#FF3700'}}><i>"Oh, you don't need to go. I got you this."</i></span><br/>
+          <br/>
 
-
-          <span className='mission-emphasis'>5. USE YOUR OWN PHONE.</span> Each agent should read texts and go to websites on their own phone. Share any information you discover! Only your partner can respond to texts, but you will need to use your phone to click on sites, fill out information, and answer phone calls as they come in!<br/>
+          If they do not respond with these words, IT IS NOT MARBLE. Walk away and keep looking.<br/>
           <br/>
 
           <div className="orangeLine"/><br/>
+
+          <span className='mission-emphasis'>3. MARBLE WILL GIVE YOU SOMETHING.</span><br/>
           <br/>
 
-          <span className='mission-emphasis'>6. TO BEGIN,</span> cross the street and walk to the <span className='mission-emphasis'>RED BELL TOWER</span>.
+          <div className="orangeLine"/><br/>
 
+          <span className='mission-emphasis'>4. SAY THANKS AND WALK AWAY.</span> Do not follow as that will spook Marble.<br/>
+          <br/>
+
+          <div className="orangeLine"/><br/>
           
-          <span className='mission-emphasis'>7. Once you are there, tell your partner to text <span style={{ color: '#f9DF39'}}>READY FOR DANGER</span> to Agent Papyrus.</span>
+          <span className='mission-emphasis'>5. TEXT AGENT PAPYRUS A DESCRIPTION OF WHAT MARBLE GAVE YOU.</span><br/>
+          <br/>
+
         </div>
 
 
