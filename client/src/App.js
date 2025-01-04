d70722318f400ab9128733bb8837091f29cadfb3
Nick Johnson <nlj222@gmail.com>
2025-01-04 12:40:10 -0800

    removed lazy and suspense as it did not improve performance

--- client/src/App.js
+++ client/src/App.js
@@ -1,98 +1,96 @@
-import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 
-// Lazy load the components
-const Confirmed = lazy(() => import("./components/Confirmed"));
-const Debrief = lazy(() => import("./components/Debrief"));
-const Marble = lazy(() => import("./components/Marble"));
-const Mission = lazy(() => import("./components/Mission"));
-const Myhistory = lazy(() => import("./components/Myhistory"));
-const Myprofile = lazy(() => import("./components/Myprofile"));
-const NotFound = lazy(() => import("./components/NotFound"));
-const ObeliskTerms = lazy(() => import("./components/ObeliskTerms"));
-const Opbc = lazy(() => import("./components/Opbc"));
-const Payment = lazy(() => import("./components/Payment"));
-const Privacy = lazy(() => import("./components/Privacy"));
-const Private = lazy(() => import("./components/Private"));
-const Start = lazy(() => import("./components/Start"));
-const Terms = lazy(() => import("./components/Terms"));
-const TheMission = lazy(() => import("./components/TheMission"));
-const YourMission = lazy(() => import("./components/YourMission"));
-const YourUpdatedMission = lazy(() => import("./components/YourUpdatedMission"));
+import Confirmed from "./components/Confirmed.js";
+import Debrief from "./components/Debrief.js";
+import Marble from "./components/Marble.js";
+import Mission from "./components/Mission.js";
+import Myhistory from "./components/Myhistory.js";
+import Myprofile from "./components/Myprofile.js";
+import NotFound from "./components/NotFound.js";
+import ObeliskTerms from "./components/ObeliskTerms.js";
+import Opbc from "./components/Opbc.js";
+import Payment from "./components/Payment.js";
+import Privacy from "./components/Privacy.js";
+import Private from "./components/Private.js";
+import Start from "./components/Start.js";
+import Terms from "./components/Terms.js";
+import TheMission from "./components/TheMission.js";
+import YourMission from "./components/YourMission.js";
+import YourUpdatedMission from "./components/YourUpdatedMission.js";
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        {/* MISSION PAGES */}
-        <Switch>
-          <Route path="/start" exact>
-            <Start />
-          </Route>
-          <Route path="/confirmed" exact>
-            <Confirmed />
-          </Route>
-          <Route path="/mission" exact>
-            <Mission />
-          </Route>
-          <Route path="/yourmission" exact>
-            <YourMission />
-          </Route>
-          <Route path="/themission" exact>
-            <TheMission />
-          </Route>
-          <Route path="/myprofile" exact>
-            <Myprofile />
-          </Route>
-          <Route path="/myhistory" exact>
-            <Myhistory />
-          </Route>
-          <Route path="/marble" exact>
-            <Marble />
-          </Route>
-          <Route path="/yourupdatedmission" exact>
-            <YourUpdatedMission />
-          </Route>
-          <Route path="/opbc" exact>
-            <Opbc />
-          </Route>
-          <Route path="/obeliskterms" exact>
-            <ObeliskTerms />
-          </Route>
-          <Route path="/debrief" exact>
-            <Debrief />
-          </Route>
 
-          {/* ADMIN PAGES */}
-          {/* <Route path="/bypass" exact>
-            <Bypass />
-          </Route> */}
-          {/* <Route path="/cancel" exact>
-            <Cancel />
-          </Route> */}
+      {/* MISSION PAGES */}
+      <Switch>
+        <Route path="/start" exact>
+          <Start />
+        </Route>
+        <Route path="/confirmed" exact>
+          <Confirmed />
+        </Route>
+        <Route path="/mission" exact>
+          <Mission />
+        </Route>
+        <Route path="/yourmission" exact>
+          <YourMission />
+        </Route>
+        <Route path="/themission" exact>
+          <TheMission />
+        </Route>
+        <Route path="/myprofile" exact>
+          <Myprofile />
+        </Route>
+        <Route path="/myhistory" exact>
+          <Myhistory />
+        </Route>
+        <Route path="/marble" exact>
+          <Marble />
+        </Route>
+        <Route path="/yourupdatedmission" exact>
+          <YourUpdatedMission />
+        </Route>
+        <Route path="/opbc" exact>
+          <Opbc />
+        </Route>
+        <Route path="/obeliskterms" exact>
+          <ObeliskTerms />
+        </Route>
+        <Route path="/debrief" exact>
+          <Debrief />
+        </Route>
 
-          {/* GENERAL PAGES */}
-          <Route path="/" exact>
-            <div>MAIN LANDING PAGE</div>
-          </Route>
-          <Route path="/privacy" exact>
-            <Privacy />
-          </Route>
-          <Route path="/terms" exact>
-            <Terms />
-          </Route>
-          <Route path="/payment" exact>
-            <Payment />
-          </Route>
-          <Route path="/private" exact>
-            <Private />
-          </Route>
+        {/* ADMIN PAGES */}
+        {/* <Route path="/bypass" exact>
+          <Bypass />
+        </Route> */}
+        {/* <Route path="/cancel" exact>
+          <Cancel />
+        </Route> */}
 
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Suspense>
+        {/* GENERAL PAGES */}
+        <Route path="/" exact>
+          <div>MAIN LANDING PAGE</div>
+        </Route>
+        <Route path="/privacy" exact>
+          <Privacy />
+        </Route>
+        <Route path="/terms" exact>
+          <Terms />
+        </Route>
+        <Route path="/payment" exact>
+          <Payment />
+        </Route>
+        <Route path="/private" exact>
+          <Private />
+        </Route>
+
+        <Route path="*">
+          <NotFound />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
