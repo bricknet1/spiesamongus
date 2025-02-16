28560e7b5cf815c63dabe911892a76ca2fc010a8
Nick Johnson <nlj222@gmail.com>
2025-02-16 09:53:26 -0800

    added 2nd payment widget to payment page

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,11 +1,19 @@
 import Questions from "./Questions.js";
 import SocialFooter from "./SocialFooter.js";
 
-function BuyButtonComponent() {
-  // Paste the stripe-buy-button snippet in your React component
+function TopBuyButtonComponent() {
   return (
     <stripe-buy-button
       buy-button-id="buy_btn_1QaPo7EtWxX2z5rqaHSu0Ogp"
+      publishable-key="pk_live_DMm4bI0jUPKs4HUd2LVxZTTF"
+    ></stripe-buy-button>
+  );
+}
+
+function BottomBuyButtonComponent() {
+  return (
+    <stripe-buy-button
+      buy-button-id="buy_btn_1PfrA9EtWxX2z5rqufPkW4f6"
       publishable-key="pk_live_DMm4bI0jUPKs4HUd2LVxZTTF"
     ></stripe-buy-button>
   );
@@ -38,12 +46,11 @@ function Payment() {
         <br />
         <br />
 
-        <BuyButtonComponent />
+        <TopBuyButtonComponent />
 
         <br />
         <br />
         <br />
-
       </div>
 
       <div className="orangeBar">GENERAL PAYMENTS</div>
@@ -66,7 +73,7 @@ function Payment() {
         <br />
         <br />
         <br />
-        WIDGET HERE?
+        <BottomBuyButtonComponent />
         <br />
         <br />
         <br />
