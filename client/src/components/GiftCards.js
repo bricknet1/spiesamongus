b1bd86474ce7ed75b1b8655186323d5e2d7f79ba
Nick Johnson <nlj222@gmail.com>
2025-07-31 19:25:15 -0700

    added a Redeem  section to giftcards and also some style changes to that page

--- client/src/components/GiftCards.js
+++ client/src/components/GiftCards.js
@@ -31,8 +31,23 @@ function GiftCards() {
   };
 
   const giftCardPicStyle = {
-    width: isMobile ? "80vw": "700px"
+    width: isMobile ? "64vw": "560px"
   };
+
+  const olStyle = {
+    paddingLeft: isMobile ? "10vw" : "80px",
+  }
+
+  const redeemInstructionsStyle = {
+    fontSize: isMobile ? "6.5vw": "40px",
+    fontWeight:"bold",
+    lineHeight: isMobile ? "10vw" : "50px",
+    textAlign:"left",
+    width: isMobile ? "90vw" : "1100px",
+    margin: isMobile ? "" : "0 auto",
+    paddingLeft: isMobile ? "5vw" : "",
+    paddingTop: isMobile ? "5vw" : "50px"
+  }
 
   return (
     <div className="pageContent" style={pageContentStyle}>
@@ -50,18 +65,25 @@ function GiftCards() {
         <span style={boldStyle}>Give the gift of espionage with a<br/><a target="_blank" rel="noopener noreferrer" style={{"color":"#F9DF39"}} href="https://offthecouch.io/book/spies/gift-card">spy mission gift card</a>!</span><br/>
         <br/>
         <a target="_blank" rel="noopener noreferrer" href="https://offthecouch.io/book/spies/gift-card"><img src={giftCardPic} style={giftCardPicStyle} alt="Gift Card"/></a>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <div className={orangeBar}>REDEEM GIFT CARDS</div>
+      <div style={redeemInstructionsStyle}>
+        To redeem a Spies Among Us Gift Card:
+        <ol style={olStyle}>
+          <li>Go to our <a target="_blank" rel="noopener noreferrer" href="https://offthecouch.io/book/spies/gift-card" style={{"color":"#F9DF39"}}>ticketing site.</a></li>
+          <li>Pick a day and time for your mission.</li>
+          <li>Click through all the way to checkout. (ignore the promo code box)</li>
+          <li>Change payment type to "gift card" and enter your gift card card code.</li>
+        </ol>
+      <br/>
 
-        </div>
+      </div>
 
         <Questions />
 
