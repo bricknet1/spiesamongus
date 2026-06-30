a13ca00dedd0976e0984b70627bc0a7645897591
Nick Johnson <nlj222@gmail.com>
2026-06-29 18:46:43 -0700

    Added Seattle phone number in the Leslie Footer

--- client/src/components/LeslieFooter.js
+++ client/src/components/LeslieFooter.js
@@ -1,6 +1,12 @@
 import lesliePic from '../assets/pictures/Leslie.jpg';
+import { useSubdomain } from './SubdomainProvider.js';
+
+const LESLIE_PHONE_APP = '+18778477437';
+const LESLIE_PHONE_SEATTLE = '+18773275007';
 
 function LeslieFooter({unfixed}) {
+  const subdomain = useSubdomain();
+  const lesliePhone = subdomain === 'seattle' ? LESLIE_PHONE_SEATTLE : LESLIE_PHONE_APP;
 
   return (
     <div className={unfixed ? null : 'leslieFooter'}> {/* will be a fixed position by default unless "true" is passed in */}
@@ -11,7 +17,7 @@ function LeslieFooter({unfixed}) {
         </div>
 
         <div className="leslieFooterText">
-          Having a catastrophic issue with your mission? Text <a className='textLeslieLink' target="_blank" rel="noreferrer" href="sms:+18778477437"><u>Agent Leslie</u></a> for help!
+          Having a catastrophic issue with your mission? Text <a className='textLeslieLink' target="_blank" rel="noreferrer" href={`sms:${lesliePhone}`}><u>Agent Leslie</u></a> for help!
         </div>
       </div>
     </div>
